86d7384f2adea651bb77a5654cb7b88824130557
Luan Rodrigues <35808867+luax7@users.noreply.github.com>
2026-05-03 00:47:57 -0300

    Site source stating

---
+++ SiteSource/ArkansasSource/src/spell.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let name;
+    export let lvl;
+    export let ShortDesc;
+    export let components;
+</script>
+
+<div class="SpellContainer">
+    <h1><spam>{name}</spam>, level {lvl}</h1>
+    <p>{ShortDesc}</p>
+    <p>Components: {components}</p>
+</div>

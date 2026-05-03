82b64df90f325f8f26869fc5c3fc577c9069101f
Luan Rodrigues <35808867+luax7@users.noreply.github.com>
2026-05-03 01:07:15 -0300

    Starting to pull info

---
+++ SiteSource/ArkansasSource/src/routes/SpellList/+page.svelte
@@ -0,0 +1,20 @@
+<div class="MainContainer">
+    <div class="Spells">
+
+    </div>
+    <div class="ActionBar">
+
+    </div>
+</div>
+<script>
+    
+    async function GetSpells()
+    {
+        return await fetch("https://api.github.com/repos/luax7/ArkansasTTRPG/tree/master/Sistema/Magias")
+    }
+    function FilterSpells()
+    {
+
+    }
+
+</script>
--- SiteSource/ArkansasSource/src/spell.svelte
+++ SiteSource/ArkansasSource/src/spell.svelte
@@ -1,12 +1,18 @@
 <script>
-    export let name;
-    export let lvl;
-    export let ShortDesc;
-    export let components;
+    let {
+        name,
+        level,
+        components,
+        shortdesc,
+    } = $props();
 </script>
 
 <div class="SpellContainer">
-    <h1><spam>{name}</spam>, level {lvl}</h1>
-    <p>{ShortDesc}</p>
+    <h1><spam>{name}</spam>, level {level}</h1>
+    <p>{shortdesc}</p>
     <p>Components: {components}</p>
 </div>
+
+<style>
+    
+</style>

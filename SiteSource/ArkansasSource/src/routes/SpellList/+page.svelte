7bb59a18071c7d359e44bb749f82498e36602e47
Luan Rodrigues <35808867+luax7@users.noreply.github.com>
2026-05-06 14:02:49 -0300

    ...

--- SiteSource/ArkansasSource/src/routes/SpellList/+page.svelte
+++ SiteSource/ArkansasSource/src/routes/SpellList/+page.svelte
@@ -1,20 +1,214 @@
 <div class="MainContainer">
-    <div class="Spells">
-
-    </div>
-    <div class="ActionBar">
-
-    </div>
-</div>
-<script>
     
-    async function GetSpells()
-    {
-        return await fetch("https://api.github.com/repos/luax7/ArkansasTTRPG/tree/master/Sistema/Magias")
-    }
-    function FilterSpells()
-    {
+        {#each FilteredSpells.values() as Spells}
+            <Spell 
+            components={Spells.Props.componentes}
+            level={Spells.Level}
+            name={Spells.Name}
+            shortdesc={Spells.Description.slice(0,60)+"..."}
+            custo={Spells.Props.custo}>
 
+            </Spell>
+        {/each}
+    
+</div>
+<div class="BottomBar">
+    <ul class="QueryButtons">
+        <li id="FilterButton"><button>filter</button></li>
+        <li id="SearchInput"><input bind:value={TextField} type="text"></li>
+    </ul>
+    <ul class="selectedButton">
+        <li><button>Selected</button></li>
+    </ul>
+</div>
+<script lang="ts">
+    import Spell from "../../spell.svelte";
+
+let FetchedSpellsAPI : FetchSpellResult[] = []; 
+let DownloadedSpells : SpellClass[] = $state([]);
+let Filters: ((spell: SpellClass) => boolean)[] = 
+[
+((spell) => {
+    if(TextField != "") return spell.Name.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").includes(TextField.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, ""))
+    return true
+})
+];
+
+
+let FilteredSpells : SpellClass[] = $derived(DownloadedSpells.filter(spell => Filters.every(fn => fn(spell))))
+
+
+
+
+let TextField : string = $state("");
+
+
+    import yaml from 'js-yaml'
+    import { onMount } from "svelte";
+
+    onMount(async() => {
+        FetchedSpellsAPI.push(...(await GetSpellsInfoFromLevel(1)))
+        FetchedSpellsAPI.push(...(await GetSpellsInfoFromLevel(2)))
+
+        LoadFilteredSpells()
+        .then(() => {console.log(DownloadedSpells)})
+    })
+
+    async function GetSpellsInfoFromLevel(level : number) : Promise<FetchSpellResult[]>
+    {
+        return new Promise(async(res,rej) => {
+            let rest : string;
+        
+            rest = (await (await fetch("https://api.github.com/repos/luax7/ArkansasTTRPG/contents/Sistema/Magias/Nivel%20"+level.toString())).text())
+            const response = JSON.parse(rest) as FetchSpellResult[]
+        
+            res(response)
+        })
+
+        
+    }
+
+    async function GetSpell(url:string) : Promise<SpellClass> 
+    {
+        return new Promise<SpellClass>(async(resolve,reject) => {
+            
+
+            (await fetch(url)).text().then((SpellRaw) => {
+                
+            const Blocks = SpellRaw.split("---").slice(1)
+            const Properties = yaml.load(Blocks.at(0)?.toLowerCase()!)
+            const Desc = Blocks.at(1)
+            const Name = url.split("/").slice(-1).join("").slice(0,-3).split("%20").join(" ")
+            
+            resolve(new SpellClass(
+                (Desc?.startsWith("##") ? Desc.slice(13) : Desc) || "",
+                Properties as SpellProperties,
+                Name,
+                url,
+                url.split("/").slice(-2,-1).join("").split("%20")[1]))
+        })
+        .catch((err) => {
+            console.log(err)
+            return
+        })
+
+            
+        })
+
+    }
+    async function LoadFilteredSpells()
+    {
+        FetchedSpellsAPI.forEach(async(value) => {
+            (await GetSpell(value.download_url)).Save()
+        })
+
+        DownloadedSpells.sort((a,b) => {return Number(a.Level)-Number(b.Level)})
+    }
+
+    type FetchSpellResult ={
+    "name": string,
+    "path": string,
+    "sha": string,
+    "size": number,
+    "url": string,
+    "html_url": string,
+    "git_url": string,
+    "download_url": string,
+    "type": "file",
+        "_links": {
+          "self": string,
+          "git": string,
+          "html": string
+        }
+    }
+    class SpellProperties {
+        "custo":number
+        "Tipo":string
+        "duração":string
+        "alcance":string
+        "dlvo":string
+        "dificuldade":string
+        "açao":string
+        "componentes":string
+    }
+    class SpellClass {
+        constructor(desc:string,props:SpellProperties,Name : string,Url:string,Level:string)
+        {
+            this.Description = desc;
+            this.Props = props
+            this.Name = Name
+            this.Url = Url
+            this.Level=Level
+        }
+        Props:SpellProperties = new SpellProperties
+        Description:string = ""
+        Name:string
+        Url:string
+        Level:string
+
+        public Update() : Promise<boolean>{
+            return new Promise<boolean>((resolve,reject) => {
+                GetSpell(this.Url)
+                .then((newValue) => {
+
+                    this.Description = newValue.Description;
+                    this.Name = newValue.Name;
+                    this.Props = newValue.Props;
+                    this.Save()
+                    
+                    resolve(true)
+                })
+                .catch((err) => {
+                    reject(err)
+                })
+            })
+        }
+        public Save()
+        {
+            const index = DownloadedSpells.findIndex((Query,index) => {return Query.Name == this.Name})
+            console.log(this)
+            if(index == -1) DownloadedSpells.push(this)
+            else DownloadedSpells.at(index) == this
+        }
     }
 
 </script>
+<style>
+
+    .MainContainer{
+        width: 100%;
+        height: 90vh;
+        display: grid;
+        border: 3px black;
+        border-radius: 10px;
+        overflow-y: scroll;
+        overflow-x: auto;
+        grid-template-columns: repeat(2,1fr);
+        gap:10px 0px;
+        align-content: start;
+        max-width: 1240px;
+    }
+    .BottomBar{
+        width: 100vw;
+        height: 10vh;
+        background-color: rgb(1, 2, 22);
+        color: white;
+        display: flex;
+        justify-content: space-around;
+    }
+    .QueryButtons{  
+        display: flex;
+        list-style: none;
+        justify-self: start;
+        align-self: center;
+    }
+    .QueryButtons *{
+        margin-left: 10px;
+    }
+    .selectedButton{
+        align-self:center;
+        justify-self: flex-end;
+        list-style: none;
+    }
+
+</style>
--- SiteSource/ArkansasSource/src/spell.svelte
+++ SiteSource/ArkansasSource/src/spell.svelte
@@ -4,15 +4,67 @@
         level,
         components,
         shortdesc,
+        custo
     } = $props();
 </script>
 
 <div class="SpellContainer">
-    <h1><spam>{name}</spam>, level {level}</h1>
-    <p>{shortdesc}</p>
-    <p>Components: {components}</p>
+    <h1 class="MainInfo"><spam class="SpammedName">{name}</spam>, level {level}</h1>
+    <p class="shortdesc">{shortdesc}</p>
+
+    <div class="flexend">
+            <p>Custo: <span class="bold">{custo}</span></p>
+            <p>Components: <span class="bold">{components}</span></p>
+    </div>
 </div>
 
 <style>
-    
+    .bold{
+        font-style: italic;
+        font-weight: bold;
+    }
+    .shortdesc{
+            -webkit-mask-image: linear-gradient(to right, rgba(255,255,255,1) 0%,rgba(255,255,255,1) 95%,rgba(0,0,0,1) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+            mask-image: linear-gradient(to right, rgba(255,255,255,1) 0%,rgba(255,255,255,1) 95%,rgba(0,0,0,1) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+
+    }
+
+    .flexend{
+        margin-top: auto !important;
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 10px !important;
+    }
+    .SpellContainer{
+        width: calc(100% - 20px);
+        min-width: 200px;
+        height: 150px;
+        border: black 2px solid;
+        border-radius: 10px;
+        margin: 10px;
+        display: flex;
+        flex-direction: column;
+        font-family: 'Courier New', Courier, monospace;
+        max-width: 580px;
+        padding: 0px;
+    }
+    .SpellContainer * {
+        margin: 0px;
+    }
+    .SpellContainer h1 {
+        margin-top: 10px;
+        margin-left: 10px;
+    }
+    .SpellContainer p{
+        margin-left: 10px;
+    }
+    .MainInfo{
+        font-size: 1rem !important;
+    }
+    .SpammedName{
+        font-size: 2rem;
+    }
+    .SpellContainer:hover{
+        cursor: pointer;
+    }
 </style>
